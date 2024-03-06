c242fbecb3531c71810d1d76fa525dffe66e7d59
aljohara147 <159161353+aljohara147@users.noreply.github.com>
2024-03-06 15:19:17 +0300

    Add files via upload

---
+++ webLab4/webLab4/webLab4/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from calctaxx import views
+
+urlpatterns = [
+    path('', views.default_view),
+    path('<int:number>/', views.calculate_tax),
+    path('taxrate/', views.tax_rate_view),
+    path('admin/', admin.site.urls),
+]
+
+
+
+
+
